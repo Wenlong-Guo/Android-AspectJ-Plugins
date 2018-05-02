af373b4e373011d8ec8c7b53f8e54b6d9b148f22
郭文龙 <guowenlong20000@sina.com>
2018-05-02 16:54:45 +0800

    修改了一下包名 😊

---
+++ app/src/main/java/me/guowenlong/gradlepluginsdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package me.guowenlong.gradlepluginsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.aspectj.lang.annotation.Aspect;
+
+import me.guowenlong.gradlepluginsdemo.Animals;
+
+/**
+ * 测试页面
+ */
+@Aspect
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Animals().fly();
+    }
+}
